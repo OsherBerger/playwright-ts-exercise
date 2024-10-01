bd1d104eb48a8140ce84da02b8a8432ccdb8c97e
your-name <osherberger@gmail.com>
2024-10-01 13:30:48 +0300

    better code practices and more tests

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
+import LoginPage from '../pages/LoginPage';
+import UserCredentials from '../helpers/UserCredentials';
+
 
 test('sanity test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-//   await page.locator('[data-test="login-credentials"]').click();
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="username"]').press('Tab');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+
+  const loginPage = new LoginPage(page);
+  await loginPage.loginToApplication(UserCredentials.PERFORMANCE_GLITCH_USER, UserCredentials.CORRECT_PASSWORD);
+
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
@@ -26,4 +27,21 @@ test('sanity test', async ({ page }) => {
   await page.getByRole('button', { name: 'Open Menu' }).click();
   await page.locator('[data-test="reset-sidebar-link"]').click();
   await page.locator('[data-test="logout-sidebar-link"]').click();
+
+});
+
+test('demo test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  const loginPage = new LoginPage(page);
+  await loginPage.loginToApplication(UserCredentials.LOCKED_OUT_USER, UserCredentials.CORRECT_PASSWORD);
+
+});
+
+test('demo test 2', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  
+    const loginPage = new LoginPage(page);
+    await loginPage.loginToApplication(UserCredentials.STANDARD_USER, UserCredentials.CORRECT_PASSWORD);
+
 });
